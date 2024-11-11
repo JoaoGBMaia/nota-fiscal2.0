7cafc59e6cb847d2d48fcb89684f76676904a847
Joao Gabriel Maia <joaogabrielmaia.ti@gmail.com>
2024-11-11 11:55:54 -0300

    Add files via upload

---
+++ nota fiscal 2.0/script.js
@@ -0,0 +1,16 @@
+const nfDatabase = {
+    "972": {
+        data: "12/10/2024",
+        produto: "Polo Pima White M",
+        observacao: "Nota deu entrada",
+        de: "Trancoso",
+        para: "Leblon"
+    },
+    "973": {
+        data: "13/10/2024",
+        produto: "Camisa Polo Preta G",
+        observacao: "Aguardando entrega",
+        de: "São Paulo",
+        para: "Rio de Janeiro"
+    }
+};
